0d868fdea4df33d2bc02f60204f27ee3a776529d
honggu12 <gu4734@hanmail.net>
2020-09-15 13:02:33 +0900

    참조 자료형 연습

--- src/com/javaex/reftypes/EnumEx.java
+++ src/com/javaex/reftypes/EnumEx.java
@@ -5,12 +5,12 @@ public class EnumEx {
 	public static void main(String[] args) {
 //		enumEx1();
 		enumEx2();
-	}
+	}	
 	
 	public static void enumEx2() {
-		// ConditionalEx의 switchEx3 메서드의 enum 버전
+		//	ConditionalEx의 switchEx3 메서드의 enum 버전
 		String day = "FRIDAY";
-		Week dayEnum = Week.valueOf(day);	// String -> Week 변환
+		Week dayEnum = Week.valueOf(day);	//	String -> Week 변환
 		String act;
 		
 		switch (dayEnum) {
@@ -34,18 +34,19 @@ public class EnumEx {
 		}
 		System.out.println(day + "에는 " + act);
 	}
+	
 	public static void enumEx1() {
-		// 열거 상수 활용하기
+		//	열거 상수 활용하기
 		Week today = Week.TUESDAY;
-		System.out.printf("Today is %s(%d)%n",
+		System.out.printf("Today is %s(%d)%n", 
 				today.name(),
 				today.ordinal());
 		
-		// 문자열 -> 열거 객체로 변환
-		String day = "FRIDAY";
-		Week obj = Week.valueOf(day);
-		System.out.print(obj.name() + " " + obj.ordinal());
-		//.
+		//	문자열 -> 열거 객체로 변환
+		String day = "FRIDAY";	
+		Week obj = Week.valueOf(day);	
+		System.out.println(obj.name() + " " + obj.ordinal());
+				
 	}
 
 }
